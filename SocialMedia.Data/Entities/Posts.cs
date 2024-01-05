579c2bb1db99995ac299627a04685c3e3cda8b0e
ramontgomery <ryan.andrew.montgomery@gmail.com>
2024-01-04 21:47:27 -0500

    posts

--- SocialMedia.Data/Entities/Posts.cs
+++ SocialMedia.Data/Entities/Posts.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,15 @@ namespace SocialMedia.Data.Entities
 
 
         public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        [ForeignKey]
+        public int AuthorId { get; set; }
+        [ForeignKey]
+        public int PostId { get; set; }
+        public List<Comments> Comments = new List<Comments>();
+
+        public int MyProperty { get; set; }
 
     }
 }
